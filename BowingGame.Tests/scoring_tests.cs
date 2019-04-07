f6b51b936ef56725dbf024e79577301d74ccf118
Marshall Offutt <marshalloffutt@gmail.com>
2019-04-07 12:26:54 -0500

    Can roll spares

--- BowingGame.Tests/scoring_tests.cs
+++ BowingGame.Tests/scoring_tests.cs
@@ -5,6 +5,11 @@ namespace BowingGameScoring.Tests
 {
     public class scoring_tests
     {
+        [Fact]
+        public void test01_can_make_a_new_game()
+        {
+            var game = new BowlingGame();
+        }
 
         [Fact]
         public void test02_can_roll_gutter_game()
@@ -28,6 +33,19 @@ namespace BowingGameScoring.Tests
             game.RollMany(1, 20);
 
             Assert.Equal(20, game.Score);
+        }
+
+        [Fact]
+        public void test04_can_roll_a_spare()
+        {
+            var game = new BowlingGame();
+
+            game.Roll(7);
+            game.Roll(3);
+            game.Roll(4);
+            game.RollMany(0, 17);
+
+            Assert.Equal(18, game.Score);
         }
 
     }
--- BowingGameScoring/BowlingGame.cs
+++ BowingGameScoring/BowlingGame.cs
@@ -1,21 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BowingGameScoring
 {
     public class BowlingGame
     {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
         //Need a score function that returns an integer
-        public int Score { get; private set; }
+        public int Score
+        {
+            get
+            {
+                var score = 0;
+                var rollIndex = 0;
+
+                //Loop over frames
+                for (var frame = 0; frame < 10; frame++)
+                {
+                    //Check to see if a spare is rolled
+                    if (IsSpare(rollIndex))
+                    {
+                        score += GetSpareScore(rollIndex);
+                    }
+                    else
+                    {
+                        score += GetStandardScore(rollIndex);
+                    }
+
+                    rollIndex += 2;
+                }
+
+                return score;
+            }
+        }
+
+        private int GetStandardScore(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1];
+        }
+
+        private int GetSpareScore(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        }
+
+        private bool IsSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+        }
 
         //Need a roll function that returns an integer
         public void Roll(int pins)
         {
-            Score += pins;
+            rolls[currentRoll++] = pins;
         }
 
-        //Roll all the frames
+        //Multiple rolls
         public void RollMany(int pins, int rolls)
         {
             for (var i = 0; i < rolls; i++)
