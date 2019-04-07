4be8b54225622766d25a7170df8a1a3f93e9fd03
Marshall Offutt <marshalloffutt@gmail.com>
2019-04-07 10:46:00 -0500

    refactor

--- BowingGame.Tests/scoring_tests.cs
+++ BowingGame.Tests/scoring_tests.cs
@@ -5,25 +5,26 @@ namespace BowingGameScoring.Tests
 {
     public class scoring_tests
     {
-        [Fact]
-        public void can_make_new_game()
-        {
-            //Arrange--context--given
-            var game = new BowlingGame();
-        }
 
         [Fact]
-        public void can_roll_gutter_game()
+        public void test02_can_roll_gutter_game()
         {
             //Arrange--context--given
             var game = new BowlingGame();
 
             //Act--do the thing--when
+            //20 total rolls in a bowling game
             for (var i = 0; i < 20; i++)
                 game.Roll(0);
 
             //Assert--check the result--then
             Assert.Equal(0, game.Score);
+        }
+
+        [Fact]
+        public void test03_can_roll_all_ones()
+        {
+
         }
     }
 }
